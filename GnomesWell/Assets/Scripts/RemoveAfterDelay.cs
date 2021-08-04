33ef29dcd1f151dce83e4622c07582e849ffed30
Dorian Jarosz <deltian1989@gmail.com>
2021-08-04 20:14:55 +0200

    Remove after delay

--- GnomesWell/Assets/Scripts/RemoveAfterDelay.cs
+++ GnomesWell/Assets/Scripts/RemoveAfterDelay.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class RemoveAfterDelay : MonoBehaviour
 {
+    public float delay = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine("Remove");
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Remove()
     {
-        
+        yield return new WaitForSeconds(delay);
+        Destroy(gameObject);
     }
 }
